4e6f48bb0d212ca3c7716a29bb2f63e221141e08
Scatmandu <mcclur05@nsuok.edu>
2020-09-16 18:36:21 +0000

    Added 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main- void
+* description: printf's a string
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
