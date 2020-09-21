18422b2fae5c618520a9d877b96d501a7a6ddf25
Scatmandu <mcclur05@nsuok.edu>
2020-09-21 16:59:29 +0000

    added file 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* _isalpha- tests if an int is a letter of the alphabet
+* @c: an int
+* description- tests if an int is a letter of the alphabet
+* Return: 1 or 0
 **/
 int _isalpha(int c)
 {
