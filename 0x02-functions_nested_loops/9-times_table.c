b364e6ba3d83d73ccd74fb9acff3ff01af8a6913
Scatmandu <mcclur05@nsuok.edu>
2020-09-22 23:05:29 +0000

    added file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,36 +6,42 @@
 **/
 void times_table(void)
 {
-	int n1, n2;
+	int n1;
+	int n2;
+	int mult;
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = 0; n2 < 10; n2++)
 		{
-			if (((n1 * n2) < 10) && (n2 != 9))
-			{
-				_putchar('0' + (n1 * n2));
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (((n1 * n2) >= 10) && (n2 != 9))
-			{
-				_putchar('0' + ((n1 * n2) / 10));
-				_putchar('0' + ((n1 * n2) % 10));
-				_putchar(',');
-				_putchar(' ');
-			}
+			mult = n1 * n2;
 
-			else if ((n2 > 8) && ((n2 * n1) < 10))
+			if (((mult) < 10) && (n2 != 9))
 			{
-				_putchar('0' + (n1 * n2));
+				_putchar('0' + (mult));
+				_putchar(',');
+				_putchar(' ');
+				if (((n2 + 1) * n1) < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			else if (((mult) >= 10) && (n2 != 9))
+			{
+				_putchar('0' + ((mult) / 10));
+				_putchar('0' + ((mult) % 10));
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if ((n2 > 8) && ((mult) < 10))
+			{
+				_putchar('0' + (mult));
 				_putchar('\n');
 			}
-			else if ((n2 > 8) && ((n2 * n1) > 10))
+			else if ((n2 > 8) && ((mult) > 10))
 			{
-				_putchar('0' + ((n1 * n2) / 10));
-				_putchar('0' + ((n1 * n2) % 10));
+				_putchar('0' + ((mult) / 10));
+				_putchar('0' + ((mult) % 10));
 				_putchar('\n');
 			}
 		}
