ee44770b267930ae4fbfb64323c7f0d0cdc8f9a1
Scatmandu <mcclur05@nsuok.edu>
2020-09-23 00:58:06 +0000

    added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* add- entry point
+* description: sums two ints
+* @n1: int 1
+* @n2: int 2
+* Return: null
+**/
+int add(int n1, int n2)
+{
+	int n = n1 + n2;
+
+	return (n);
+}
