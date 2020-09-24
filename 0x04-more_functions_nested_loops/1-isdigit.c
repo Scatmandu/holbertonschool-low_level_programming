4579f7cb7f36f28cf2f3894028b8e212ac98e7a8
Scatmandu <mcclur05@nsuok.edu>
2020-09-24 17:42:36 +0000

    Added file 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+* _isdigit- entry point
+* @c: an int
+* description: checks if a digit is between 0 and 9
+* Return: null
+**/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
