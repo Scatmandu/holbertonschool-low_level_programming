ebae6760e5c4dc6f3b354e3198e84ba52577dcb9
Scatmandu <mcclur05@nsuok.edu>
2020-09-24 20:29:30 +0000

    Added file 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+*main- entry point
+*description: prints 1-100, unless n%3=0, n%5=0, or n%3 and n%5=0
+*Return: 0
+**/
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (((n % 5) == 0) && ((n % 3) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		if (n == 100)
+		{
+			putchar('\n');
+		}
+	}
+return (0);
+}
