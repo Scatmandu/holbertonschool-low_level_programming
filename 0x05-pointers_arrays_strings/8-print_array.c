f0ce02961b72f3e8335b4b943664da5a126a8033
Scatmandu <mcclur05@nsuok.edu>
2020-09-29 22:59:17 +0000

    tweaked 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 /**
 *print_array- entry point
 *description: prints array contents
-*@a: an arry
+*@a: an array
 *@n: number of elements to print
 *Return: null
 **/
@@ -10,7 +10,7 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i != n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i != n - 1)
