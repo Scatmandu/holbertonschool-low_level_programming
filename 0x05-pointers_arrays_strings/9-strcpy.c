54440310d3ccf965f60ec96ab06d637a32690840
Scatmandu <mcclur05@nsuok.edu>
2020-09-29 22:35:37 +0000

    added file 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*_strcpy- entry point
+*description: copies a string
+*@src:string to copy
+*@dest: copied string
+*Return: dest
+**/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+return (dest);
+}
