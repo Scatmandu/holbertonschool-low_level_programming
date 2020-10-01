f2bdce3f9a3707754f976cb6fb035bc362037485
Scatmandu <mcclur05@nsuok.edu>
2020-10-01 15:53:31 +0000

    Added files 0, _putchar, header, and README

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*
+*
+*
+*
+*
+**/
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; i++, j++)
+	{
+		dest[i] = src[j];
+	}
+dest[i + 1] = '\0';
+return (dest);
+}
