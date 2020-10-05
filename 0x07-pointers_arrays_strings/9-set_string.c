7c9aa7f815bf8b1a5a6636a55e7cd55868722727
Scatmandu <mcclur05@nsuok.edu>
2020-10-05 22:54:19 +0000

    added file 9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*set_string- entry point
+*@s: string to replace to with
+*@to: string to be replaced
+*Return: null
+**/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
