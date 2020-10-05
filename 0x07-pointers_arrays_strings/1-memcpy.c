4d372577d789aa12430d838b385a40ea380bd986
Scatmandu <mcclur05@nsuok.edu>
2020-10-05 18:30:29 +0000

    Added file 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+**_memcpy- entry point
+*@dest: string to copy into
+*@src: string to copy
+*@n: amount of string to copy
+*Return: dest
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		src[i] = dest[i];
+	}
+return (dest);
+}
