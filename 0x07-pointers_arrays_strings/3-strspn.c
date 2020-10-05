5fb85454a6ed9d165e334b016a5fdee060ec6373
Scatmandu <mcclur05@nsuok.edu>
2020-10-05 20:42:21 +0000

    Added file 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*_strspn- entry point
+*@s: string to compare accept to
+*@accept: string to compare s to
+*Return: k
+**/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				k++;
+				i++;
+			}
+		}
+	}
+return (k);
+}
