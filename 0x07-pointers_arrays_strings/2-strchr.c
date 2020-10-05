a401efe131e7a6107ad4200647ad189f1faf9bae
Scatmandu <mcclur05@nsuok.edu>
2020-10-05 20:04:43 +0000

    Added file 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+**_strchr - entry point
+*@s: string to print part of
+*@c: point to begin returning at
+*Return: either a string or NULL
+**/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+return (NULL);
+}
