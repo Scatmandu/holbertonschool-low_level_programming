8ffe570ed2973a45653c833a44132aed151eea9b
Scatmandu <mcclur05@nsuok.edu>
2020-10-05 18:19:17 +0000

    Added file 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+**_memset- entry point
+*@s: pointer to a string
+*@b: letter to populate string with
+*@n: amount of b's to copy to s
+*Return: s
+**/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		s[i] = b;
+	}
+return (s);
+}
