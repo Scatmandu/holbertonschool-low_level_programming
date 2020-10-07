4848d6b93dda1772f74e961762984ea3127d304f
Scatmandu <mcclur05@nsuok.edu>
2020-10-07 15:55:00 +0000

    added file 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*_print_rev_recursion- reverses a string using recursion
+*@s: a string
+*Return: void
+**/
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+
+		if (*s == s[1])
+		{
+			_putchar('\n');
+			return;
+		}
+	}
+}
