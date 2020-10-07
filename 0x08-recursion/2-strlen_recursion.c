6bbaa4d511f419e2871ef4ffbd182989702caa75
Scatmandu <mcclur05@nsuok.edu>
2020-10-07 18:07:53 +0000

    forgot betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
-*
-*
-*
+*_strlen_recursion- gets the length of a string
+*@s: a string to count
+*Return: void
 **/
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 +_strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
