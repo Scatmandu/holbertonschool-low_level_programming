77e8c7fd475c14e7aabcb8dab4460c13774682b5
Scatmandu <mcclur05@nsuok.edu>
2020-10-08 02:34:09 +0000

    Added file 100

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*wildcmp- compares two strings with * as a wildcard
+*@s1: string without wilds to compare to
+*@s2: string with wilds to compare
+*Return: 1 or 0
+**/
+int wildcmp(char *s1, char *s2)
+{
+
+}
