3a764e93db986ceffa1c8a9ed7366806fbf9f7fc
Scatmandu <mcclur05@nsuok.edu>
2020-10-09 21:01:06 +0000

    added file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*main- entry point
+*@argc: unused
+*@argv: string containing our program name
+*Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", argv[0]);
+return (0);
+}
