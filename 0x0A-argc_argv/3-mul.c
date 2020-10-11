ac63027af96b07a77111b1e393052928bfc557e8
Scatmandu <mcclur05@nsuok.edu>
2020-10-11 17:18:48 +0000

    added file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main- multiplies the value of two arguments
+*@argc: number of arguments
+*@argv: array containing values of argc
+*Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", mul);
+		return (0);
+	}
+}
