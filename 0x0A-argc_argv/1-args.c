3dfee858750b87f0a9e90f3aed5554bf1f06218b
Scatmandu <mcclur05@nsuok.edu>
2020-10-11 16:12:49 +0000

    added file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main- entry point
+*@argc: number of arguments
+*@argv: unused
+*Return: 0
+**/
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+	printf("%d\n", argc - 1);
+return (0);
+}
