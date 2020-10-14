c5e44039be77311b93a2daeb984d7b172fe6b8b2
Scatmandu <mcclur05@nsuok.edu>
2020-10-14 19:08:26 +0000

    widdly

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,22 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0)
 	{
-		return ('\0');
+		return (NULL);
 	}
 	else
 	{
 		str = malloc(sizeof(char) * size);
-
+		if (str == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
 			for (i = 0; i <= size; i++)
 			{
 				str[i] = c;
 			}
+		}
 	}
 return (str);
 }
