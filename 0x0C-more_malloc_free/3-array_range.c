37b3aad4309e57e72a1cbef57c0f2f3cf71a33c1
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 22:15:51 +0000

    MEMO WANT A CHECKY

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,13 +9,14 @@ int *array_range(int min, int max)
 {
 	int i;
 	int *arr;
+	int minmax = max - min + 1;
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * max - min + 1);
+	arr = malloc(sizeof(int) * minmax);
 	if (arr == NULL)
 		return (NULL);
-	for (i = min; i < max; i++, min++)
+	for (i = 0; i < minmax; i++, min++)
 	{
 		arr[i] = min;
 	}
