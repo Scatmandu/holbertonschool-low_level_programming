68e944786a8aef8166f5d04496c9f083d18f30e0
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 21:50:27 +0000

    Added file 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*array_range- displays a range of ints
+*@min: min value of a range
+*@max: max value of a range
+*Return: arr or NULL
+**/
+int *array_range(int min, int max)
+{
+	int i;
+	int *arr;
+
+	if (min > max)
+		return (NULL);
+	arr = malloc(sizeof(int) * max - min + 2);
+	if (arr == NULL)
+		return (NULL);
+	for (i = min; i <= max; i++, min++)
+	{
+		arr[i] = min;
+	}
+arr[i] = '\0';
+return (arr);
+}
