55b75a72ecdbc31dee4d309e3878252cd01ad918
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 19:48:18 +0000

    Added nunll byte to end of s3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,12 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s3 == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
-	{
 		s3[i] = s1[i];
-	}
 	for (j = 0; s2[j] != '\0' && j < n; j++, i++)
-	{
 		s3[i] = s2[j];
-	}
+s3[i] = '\0';
 return (s3);
 }
