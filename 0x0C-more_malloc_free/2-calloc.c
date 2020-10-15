ab6244b9bf1adfac719bf3f610d78fbf8fd9bedc
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 21:30:51 +0000

    compared to nmemb * size in for loop and set arr[i] = '\0'

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,9 +15,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
-		arr[i] = 0;
+		arr[i] = '\0';
 	}
 return (arr);
 }
