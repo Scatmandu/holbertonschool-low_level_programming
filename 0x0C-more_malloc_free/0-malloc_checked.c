62c98bc61f90e443f387894399b9a663fb19035d
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 15:15:51 +0000

    Added file 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*malloc_checked- checks to see if malloc succeeds
+*@b: size of a malloc call
+*Return: ptr
+**/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr != NULL)
+	{
+		return (ptr);
+	}
+	else
+	{
+		exit (98);
+	}
+}
