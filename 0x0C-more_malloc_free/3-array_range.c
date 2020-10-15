2605ef83fd90ec78f6ee4b934b3270d12498880e
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 22:01:14 +0000

    changed <= to <

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,7 +15,7 @@ int *array_range(int min, int max)
 	arr = malloc(sizeof(int) * max - min + 1);
 	if (arr == NULL)
 		return (NULL);
-	for (i = min; i <= max; i++, min++)
+	for (i = min; i < max; i++, min++)
 	{
 		arr[i] = min;
 	}
