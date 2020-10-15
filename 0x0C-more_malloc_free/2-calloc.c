9b04780373be10531717c90cf572b057bd76f797
Scatmandu <mcclur05@nsuok.edu>
2020-10-15 21:23:01 +0000

    added file 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*_calloc- cats two strings
+*@nmemb: number of elements to populate new string with
+*@size: size in bytes of each element
+*Return: arr or NULL
+**/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	unsigned int *arr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+	}
+return (arr);
+}
