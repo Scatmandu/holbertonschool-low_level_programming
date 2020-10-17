ff060e6d714081b0147c1cf74068532926c6a251
Scatmandu <mcclur05@nsuok.edu>
2020-10-17 20:40:02 +0000

    Added file 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*string_toupper- translates lowercase to upperscase in a string
+*@str: pointer to a string
+*Return: 0
+**/
+char *string_toupper(char *str)
+{
+	char *str;
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+return (0);
+}
