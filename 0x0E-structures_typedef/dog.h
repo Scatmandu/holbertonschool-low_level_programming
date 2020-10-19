6ba7046b9484110410ec0e74f055527c3c9a8cd5
Scatmandu <mcclur05@nsuok.edu>
2020-10-19 21:41:32 +0000

    updated dog.h and added file 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,11 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* struct dog- contains name age and owner of dog
+*struct dog- contains name age and owner of dog
 *@name: dog's name
 *@age: dog's age
 *@owner: dog's owner
@@ -11,5 +15,18 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
+
+typedef struct dog dog_t;
+
+int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
