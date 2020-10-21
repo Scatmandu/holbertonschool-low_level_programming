32a36c0252e61f8e6b44a3a88b6ea54db76efb1f
Scatmandu <mcclur05@nsuok.edu>
2020-10-21 21:03:42 +0000

    Forgot to account for empty string and betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,12 @@
 #include "function_pointers.h"
 /**
-*
-*
-*
-*
+*print_name- prints name through function pointer
+*@name: name to print
+*@f: pointer to function
+*Return: void
 **/
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (f != NULL)
+		(f)(name);
 }
