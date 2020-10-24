9bd20c0090f70e033fe5aba399aa355537338f53
Scatmandu <mcclur05@nsuok.edu>
2020-10-24 18:25:31 +0000

    forgot betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				printf("%s", va_arg(va_string, char *));
 		}
 		else
-			printf("(nil)");	
+			printf("(nil)");
 	}
 printf("\n");
 va_end(va_string);
