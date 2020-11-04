af8e0573e3c8044f8421d0b70ed6cef59b0c8ae1
Scatmandu <mcclur05@nsuok.edu>
2020-11-04 19:08:39 +0000

    Took new's initialization out of an else statement

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	
+	new->n = n;
+	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
@@ -23,8 +27,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while (traverse->next != NULL)
 			traverse = traverse->next;
 		traverse->next = new;
-		new->n = n;
-		new->next = NULL;
 	}
 return (new);
 }
