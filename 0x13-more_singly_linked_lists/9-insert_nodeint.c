cd84e781ebd65dea0786380cc47632fb8b915d34
Scatmandu <mcclur05@nsuok.edu>
2020-11-05 04:06:46 +0000

    Trying a hacky fix for edge case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new == NULL)
 		return (NULL);
+	if (idx == 0)
+		new = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
 		traverse = traverse->next;
