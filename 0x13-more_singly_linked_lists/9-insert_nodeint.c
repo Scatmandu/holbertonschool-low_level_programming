b701f199ac9af7f2f87a888b9a01f5d50d6ba7a0
Scatmandu <mcclur05@nsuok.edu>
2020-11-05 04:13:52 +0000

    Accounted for edge case of idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,8 +17,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	if (idx == 0)
-		new = *head;
-	for (i = 0; i < idx - 1; i++)
+	{
+		traverse->n = n;
+		return (traverse);
+	}
+	for (i = 1; i < idx; i++)
 	{
 		traverse = traverse->next;
 	}
