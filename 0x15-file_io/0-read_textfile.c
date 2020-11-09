8636bf1f2d311a5a17a4fb7c90bca2999ad1e149
Scatmandu <mcclur05@nsuok.edu>
2020-11-09 20:27:38 +0000

    Added file 0, header, and readme

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+*read_textfile- reads and prints a text file
+*@filename: pointer to file to be read
+*@letters: length to print
+*Return: 0 or return_value
+**/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	int length;
+	int value;
+
+	buf = malloc(sizeof(char) * letters);
+	fd = open(filename, O_RDONLY);
+	if (filename == NULL)
+		return (0);
+	if (fd == -1)
+		return (0);
+	length = read(fd, buf, letters);
+	if (length == -1)
+		return (0);
+	value = write(1, buf, length);
+	if (value == -1)
+		return (0);
+return (value);
+}
