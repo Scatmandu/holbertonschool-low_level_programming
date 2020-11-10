29a2121123570744f49f4a20f3d58cc17517a368
Scatmandu <mcclur05@nsuok.edu>
2020-11-10 19:41:03 +0000

    changed 2 to STDERR_FILENO in every dprintf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
 	buf = malloc(1024);
@@ -20,26 +20,26 @@ int main(int argc, char *argv[])
 	r = read(fd_copy, buf, 1024);
 	if (buf == NULL || fd_copy == -1 || r == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd_result = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	w = write(fd_result, buf, r);
 	if (w == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	close_copy = close(fd_copy);
 	close_result = close(fd_result);
 	if (close_copy == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_copy);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_copy);
 		exit(100);
 	}
 	if (close_result == -1)
 	{
-		dprintf(2, "error: Can't close fd %d\n", fd_result);
+		dprintf(STDERR_FILENO, "error: Can't close fd %d\n", fd_result);
 		exit(100);
 	}
 free(buf);
