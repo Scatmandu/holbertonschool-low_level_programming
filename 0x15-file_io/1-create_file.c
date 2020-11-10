df2074670397f2724e18dc76cfa8fe24250c665e
Scatmandu <mcclur05@nsuok.edu>
2020-11-10 00:47:52 +0000

    Added file 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+*create_file- creates a new file
+*@filename: pointer to the name of a file to create
+*@text_content: content to populate file with
+*Return: 1 or -1
+**/
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd, w;
+
+	for (i = 0; text_content[i] != '\0'; i++)
+	{}
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		w = write(fd, text_content, i);
+		if (w == -1)
+			return (-1);
+	}
+	else
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+		return (1);
+	}
+close(fd);
+return (1);
+}
