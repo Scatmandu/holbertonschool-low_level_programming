0945d8412255a4e123d255eeeb1fb43637c2806f
Scatmandu <mcclur05@nsuok.edu>
2020-12-13 22:39:33 +0000

    added file 2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_dnodeint- adds new node at beginning of 2x linked list
+ * @head: pointer to head
+ * @n: content to populate new node with
+ * Return: address of new node
+**/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->n = n;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
