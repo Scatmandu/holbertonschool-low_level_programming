12dedeb749d88721acce00a850b0034d2e2e3ab4
Scatmandu <mcclur05@nsuok.edu>
2020-12-13 22:07:24 +0000

    Added readme, header, and file 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_dlistint- prints elements of a linked list
+ * @h: pointer to head of  list
+ * Return: number of nodes
+**/
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *node = h;
+	unsigned int count = 0;
+
+	while (node != NULL)
+	{
+		printf("%u\n", node->n);
+		count++;
+		node = node->next;
+	}
+return (count);
+}
