e09f97a08c3fc1fcbc39e93dadbd5e92d1905149
Scatmandu <mcclur05@nsuok.edu>
2021-01-15 23:53:49 +0000

    changed a lot of stuff

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,8 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				current->value = strdup(value);
 				return (1);
 			}
-			if (current->next != NULL)
-				current = current->next;
+			current = current->next;
 		}
 	}
 	new_list = malloc(sizeof(hash_node_t *));
