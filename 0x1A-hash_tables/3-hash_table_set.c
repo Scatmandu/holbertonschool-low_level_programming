036a29f8c63a5d7c8d5a750b4ed484ce75640d1b
Scatmandu <mcclur05@nsuok.edu>
2021-01-16 00:21:46 +0000

    a

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_list);
 		free(new_list->key);
 	}
-	new_list->next = ht->array[index];
-	ht->array[index] = new_list;
+	new_list->next = current;
+	current = new_list;
 	return (1);
 }
