361d34afc120ff53f2d2f2af6581d8d2c03ca59e
Scatmandu <mcclur05@nsuok.edu>
2021-01-16 00:13:06 +0000

    adfasds

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,12 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new_list->key = strdup(key);
 	new_list->value = strdup(value);
-	if (current == NULL)
-	{
-		new_list->next = NULL;
-		current = new_list;
-		return (1);
-	}
 	new_list->next = current;
 	current = new_list;
 	return (1);
