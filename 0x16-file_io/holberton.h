a358bbce0501aab845c8fce0d0d1a66cce66567e
Scatmandu <mcclur05@nsuok.edu>
2021-08-17 21:55:24 +0000

    added include guard

--- 0x16-file_io/holberton.h
+++ 0x16-file_io/holberton.h
@@ -1,1 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _HOLBERTON_H_ */
