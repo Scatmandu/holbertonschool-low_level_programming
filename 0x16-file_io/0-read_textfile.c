5e85e232f2c25bcd159166978bd4d80e5f41f717
Scatmandu <mcclur05@nsuok.edu>
2021-08-17 21:38:32 +0000

    added file 0 and readme

---
+++ 0x16-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "holberton.h"
+/*
+* read_textfile - reads a text file and prints to STDOUT
+*
+*
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, re, wr;
+	char *buf;
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL || filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	re = read(fd, buf, letters);
+	if (re == -1)
+		return (0);
+
+	wr = write(STDOUT_FILENO, buf, re);
+	if (wr == -1)
+		return (0);
+
+	close(fd);
+	free(buf);
+	return (wr);
+}
