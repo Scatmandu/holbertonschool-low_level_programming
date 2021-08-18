45febadf2f2db638aa93fcac5a87c82945e888f4
Scatmandu <mcclur05@nsuok.edu>
2021-08-18 14:27:46 +0000

    fixed betty

--- 0x16-file_io/1-create_file.c
+++ 0x16-file_io/1-create_file.c
@@ -7,22 +7,22 @@
 **/
 int create_file(const char *filename, char *text_content)
 {
-        int fd, wr, i;
+	int fd, wr, i;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-        if (fd == -1)
-                return (-1);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
 
-        if (text_content != NULL)
-        {
-                for (i = 0; text_content[i] != '\0'; i++)
-                {}
-                wr = write(fd, text_content, i);
-                if (wr == -1)
-                        return (-1);
-        }
-        return (1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		{}
+		wr = write(fd, text_content, i);
+		if (wr == -1)
+			return (-1);
+	}
+	return (1);
 }
