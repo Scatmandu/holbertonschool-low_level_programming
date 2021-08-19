6a1443d5e7cc222e5be6643401069912eba39823
Scatmandu <mcclur05@nsuok.edu>
2021-08-19 03:08:11 +0000

    added file 3

---
+++ 0x16-file_io/3-cp.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+*
+*
+*
+*
+**/
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, re;
+	char buf[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	re = read(fd1, buf, 1024);
+	if (re == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while ((re = read(fd1, buf, 1024)) > 0)
+	{
+		if (write(fd2, buf, re) != re)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	close(fd1);
+	if (fd1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	close(fd2);
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
+	return (0);
+}
