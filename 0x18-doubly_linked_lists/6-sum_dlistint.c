b35359b07545b85a06a6b487aa6c347cc8effb58
Scatmandu <mcclur05@nsuok.edu>
2021-09-08 21:18:16 +0000

    added file 6

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* sum_dlistint- sums values contained in 2x linked list
+* @head: pointer to head of 2x linked list
+* Return: sum of nodes' values
+**/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *node = *head;
+	dlisitint_t *next;
+
+	while (node != NULL)
+	{
+		sum += node->n;
+		next = node->next;
+		node = next;
+	}
+	return (sum);
+}
